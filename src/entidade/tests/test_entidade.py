26f6ea65026b0583b3947e3482523a7e52785fa4
Rafael Morán <rm.veiga26@gmail.com>
2020-09-03 14:30:24 -0300

    src/entidade/tests/test_entidade.py - Implementado TestCase que testa a formatação de um CPF válido - Implementado TestCase que testa a formatação de um CNPJ válido

--- src/entidade/tests/test_entidade.py
+++ src/entidade/tests/test_entidade.py
@@ -2,7 +2,9 @@ from django.test import TestCase, Client
 from rest_framework import status
 
 from ..models import Entidade
+from utilitario import formatadores
 from utilitario.validadores import Validador
+
 
 client = Client()
 validador = Validador()
@@ -10,22 +12,30 @@ validador = Validador()
 
 class EntidadeTest(TestCase):
     def setUp(self) -> None:
+        self.entidade_cpf_valido = {
+            'nome': 'CPF Valido',
+            'cpf_cnpj': validador.gerador_cpf(),
+            'cliente': True,
+            'fornecedor': False,
+        }
+        self.entidade_cnpj_valido = {
+            'nome': 'CNPJ Valido',
+            'cpf_cnpj': validador.gerador_cnpj(),
+            'cliente': True,
+            'fornecedor': False,
+        }
         self.entidade_cpf_invalido = {
-            'nome': 'Rafael',
+            'nome': 'CPF Invalido',
             'cpf_cnpj': '12345678906',
             'cliente': True,
             'fornecedor': False,
         }
         self.entidade_cnpj_invalido = {
-            'nome': 'Empresa Teste',
+            'nome': 'CNPJ Invalido',
             'cpf_cnpj': '14891765000143',
             'cliente': True,
             'fornecedor': False,
         }
-        # Entidade.objects.create(
-        #     nome='João',
-        #     cpf_cnpj=validador.gerador_cpf()
-        # )
 
     def test_cadastro_entidade_cpf_invalido(self):
         nome_entidade = self.entidade_cpf_invalido.get('nome')
@@ -52,3 +62,36 @@ class EntidadeTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data[0], f'CPF/CNPJ Inválido: {cnpj_entidade}')
         self.assertFalse(Entidade.objects.filter(nome=nome_entidade))
+
+    def test_formato_cpf_valido(self):
+        nome_entidade = self.entidade_cpf_valido.get('nome')
+        cpf_valido = self.entidade_cpf_valido.get('cpf_cnpj')
+        cpf_formatado = formatadores.cpf_cnpj(cpf_valido)
+        response = client.post(
+            '/api/cadastros/entidades/',
+            self.entidade_cpf_valido,
+            content_type='application/json'
+        )
+
+        entidade_cpf = response.data.get('cpf_cnpj')
+        entidade = Entidade.objects.filter(nome=nome_entidade)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(entidade_cpf, cpf_formatado)
+        self.assertTrue(entidade)
+
+    def test_formato_cnpj_valido(self):
+        nome_entidade = self.entidade_cnpj_valido.get('nome')
+        cnpj_valido = self.entidade_cnpj_valido.get('cpf_cnpj')
+        cnpj_formatado = formatadores.cpf_cnpj(cnpj_valido)
+        response = client.post(
+            '/api/cadastros/entidades/',
+            self.entidade_cnpj_valido,
+            content_type='application/json'
+        )
+
+        entidade_cnpj = response.data.get('cpf_cnpj')
+        entidade = Entidade.objects.filter(nome=nome_entidade)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(entidade_cnpj, cnpj_formatado)
+        self.assertTrue(entidade)
+
