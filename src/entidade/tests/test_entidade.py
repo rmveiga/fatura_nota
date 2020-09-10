be024e8b036b4bf57cafcaf6a285f4feaceb7a3a
Rafael Morán <rm.veiga26@gmail.com>
2020-09-10 15:28:38 -0300

    src/entidade/api/serializers.py - Implementada regra de validação para verificar se algum dos campos cliente ou fornecedor foram marcados
    src/entidade/tests/test_entidade.py
    - Implementado teste de cadastro de entidade sem informar se é um cliente ou fornecedor
    - Implementado teste de edição de entidade sem informar se é um cliente ou fornecedor

--- src/entidade/tests/test_entidade.py
+++ src/entidade/tests/test_entidade.py
@@ -5,7 +5,6 @@ from ..models import Entidade
 from utilitario import formatadores
 from utilitario.validadores import Validador
 
-
 client = Client()
 validador = Validador()
 
@@ -34,6 +33,12 @@ class EntidadeTest(TestCase):
             'nome': 'CNPJ Invalido',
             'cpf_cnpj': '14891765000143',
             'cliente': True,
+            'fornecedor': False,
+        }
+        self.entidade_nao_cliente_e_nao_fornecedor = {
+            'nome': 'Não cliente e não fornecedor',
+            'cpf_cnpj': validador.gerador_cpf(),
+            'cliente': False,
             'fornecedor': False,
         }
 
@@ -74,6 +79,19 @@ class EntidadeTest(TestCase):
         self.assertEqual(response.data.get('cpf_cnpj')[0], f'CPF/CNPJ Inválido: {cpf_invalido}')
         self.assertEqual(entidade.cpf_cnpj, cpf_valido)
 
+    def test_cadastro_entidade_cnpj_invalido(self):
+        nome_entidade = self.entidade_cnpj_invalido.get('nome')
+        cnpj_entidade = self.entidade_cnpj_invalido.get('cpf_cnpj')
+        response = client.post(
+            '/api/cadastros/entidades/',
+            self.entidade_cnpj_invalido,
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data.get('cpf_cnpj')[0], f'CPF/CNPJ Inválido: {cnpj_entidade}')
+        self.assertFalse(Entidade.objects.filter(nome=nome_entidade))
+
     def test_edicao_entidade_cnpj_invalido(self):
         response = client.post(
             '/api/cadastros/entidades/',
@@ -98,16 +116,42 @@ class EntidadeTest(TestCase):
         self.assertEqual(response.data.get('cpf_cnpj')[0], f'CPF/CNPJ Inválido: {cnpj_invalido}')
         self.assertEqual(entidade.cpf_cnpj, cnpj_valido)
 
-
-    def test_cadastro_entidade_cnpj_invalido(self):
-        nome_entidade = self.entidade_cnpj_invalido.get('nome')
-        cnpj_entidade = self.entidade_cnpj_invalido.get('cpf_cnpj')
+    def test_cadastro_entidade_nao_cliente_e_nao_fornecedor(self):
+        nome_entidade = self.entidade_nao_cliente_e_nao_fornecedor.get('nome')
         response = client.post(
             '/api/cadastros/entidades/',
-            self.entidade_cnpj_invalido,
+            self.entidade_nao_cliente_e_nao_fornecedor,
             content_type='application/json'
         )
 
+        erro_esperado = 'A entidade precisa ser um cliente, fornecedor ou ambos'
+
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data.get('cpf_cnpj')[0], f'CPF/CNPJ Inválido: {cnpj_entidade}')
+        self.assertEqual(response.data.get('non_field_errors')[0], erro_esperado)
         self.assertFalse(Entidade.objects.filter(nome=nome_entidade))
+
+    def test_edicao_entidade_nao_cliente_e_nao_fornecedor(self):
+        response = client.post(
+            '/api/cadastros/entidades/',
+            self.entidade_cpf_valido,
+            content_type='application/json'
+        )
+
+        id_entidade = response.data.get('id')
+        content = {
+            'cliente': False,
+            'fornecedor': False,
+        }
+
+        response = client.patch(
+            f'/api/cadastros/entidades/{id_entidade}/',
+            content,
+            content_type='application/json'
+        )
+
+        erro_esperado = 'A entidade precisa ser um cliente, fornecedor ou ambos'
+        entidade = Entidade.objects.get(pk=id_entidade)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data.get('non_field_errors')[0], erro_esperado)
+        self.assertTrue(entidade.cliente)
